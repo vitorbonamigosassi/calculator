c60556bdc2729411528f8cdf4e9ef4459d5e2b35
Vitor <vitorbonamigosassi@gmail.com>
2024-05-20 10:23:29 -0300

    Add all operators

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@ const sendButton = document.querySelector(".sendClass");
 
 let inputArray = [];
 
+let number1, number2, operator;
+
 plusButton.addEventListener('click', () => {
     inputArray.push(+input.value);
     input.value = "";
@@ -13,12 +15,33 @@ plusButton.addEventListener('click', () => {
 
 
 const add = ([input1, input2]) => input1 + input2;
+const sub = ([input1, input2]) => input1 - input2;
+const div = ([input1, input2]) => input1 / input2;
+const mul = ([input1, input2]) => input1 * input2;
 
 sendButton.addEventListener('click', () => {
     let result = add(inputArray);
     console.log(result);
 })
 
+
+const operate = (number1, number2, operator) => {
+    switch (operator) {
+        case "+":
+            add(number1, number2)
+            break;
+        case "-":
+            sub(number1, number2)
+            break;        
+        case "/":
+            div(number1, number2)
+            break;
+        case "*":
+            sub(number1, number2)
+            break;
+    }
+    
+};
 
 
 

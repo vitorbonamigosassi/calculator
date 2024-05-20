39d1148407d2746a31815eafd2bf70842df8da71
Vitor <vitorbonamigosassi@gmail.com>
2024-05-20 09:57:16 -0300

    Add add function

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,29 @@
-console.log("Hello");
+console.clear();
+const input = document.querySelector(".inputClass");
+const plusButton = document.querySelector(".plusClass");
+const sendButton = document.querySelector(".sendClass");
+
+let inputArray = [];
+
+plusButton.addEventListener('click', () => {
+    inputArray.push(+input.value);
+    input.value = "";
+    console.log(inputArray);
+});
+
+
+const add = ([input1, input2]) => input1 + input2;
+
+sendButton.addEventListener('click', () => {
+    let result = add(inputArray);
+    console.log(result);
+})
+
+
+
+
+
+
+
+
+
